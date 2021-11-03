67d0500b1ba43c58658bf7a59d3933d6d3db53b0
doron <doron8971@gmail.com>
2021-11-04 01:22:22 +0200

    project changes

--- web/includes/dbh.inc.php
+++ web/includes/dbh.inc.php
@@ -10,7 +10,21 @@ $dBPassword = "O=d|J5ih&rz-\sqv";
 $dBName = "id17769391_dorondata";
 //O=d|J5ih&rz-\sqv
 
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+// $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+$dbopts = parse_url(getenv("DATABASE_URL"));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+               array(
+                'pdo.server' => array(
+                   'driver' => 'pgsql',
+                   'user' => $dbopts["user"],
+                   'password' => $dbopts["pass"],
+                   'host' => $dbopts["host"],
+                   'port' => $dbopts["port"],
+                   'dbname' => ltrim($dbopts["path"],'/')
+                   )
+               )
+);
 
 if (!$conn){
   die("Connection failed: " . mysqli_connect_error());
